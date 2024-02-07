ac43b1801a218d5748ee31bbfdbf1cff393dc035
“[chakripavankumar]” <[siriyalapavankumar@gmail.com]>
2024-02-07 11:10:34 +0530

    Navbar✅

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./components/navbar"
+
+function App() {
+  
+
+  return (
+    
+    <div className="w-full h-screen bg-[#F1F1F1]">
+   <Navbar/>
+    </div>
+  )
+}
+
+export default App
